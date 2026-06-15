8f0f8d15e6c57f86e00ada2f32c1f5219f621049
Tara <tara.petrovic03@gmail.com>
2026-06-14 20:00:49 -0700

    kmem_alloc done

--- h/MemoryAllocator.hpp
+++ h/MemoryAllocator.hpp
@@ -5,7 +5,29 @@
 #ifndef PROJECT_BASE_V1_1_MEMORYALLOCATOR_HPP
 #define PROJECT_BASE_V1_1_MEMORYALLOCATOR_HPP
 
+#include "../lib/hw.h"
+
+class MemoryAllocator {
+private:
+    MemoryAllocator() = default;
+
+public:
+    MemoryAllocator(const MemoryAllocator& obj) = delete;
+    MemoryAllocator(MemoryAllocator&& obj) = delete;
+
+
+    struct FreeMem {
+        size_t size; // size in blocks
+        FreeMem* next = nullptr;
+    };
+
+    static FreeMem* freeMemHead;
+    static void* kmem_alloc(size_t size);
+    static int kmem_free(void* ptr);
+    static void kinit();
+
+};
+
+
+
 #endif //PROJECT_BASE_V1_1_MEMORYALLOCATOR_HPP
-
-
-proba
--- src/MemoryAllocator.cpp
+++ src/MemoryAllocator.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "../h/MemoryAllocator.hpp"
+
+void *MemoryAllocator::kmem_alloc(size_t size) {
+    if (size == 0) return nullptr; // nothing needs to be allocated;
+    FreeMem* curr = freeMemHead, *prev = nullptr;
+    for (; curr != nullptr; prev = curr, curr = curr->next){
+        if (curr->size >= size) break;
+    }
+
+    if (curr == nullptr) return nullptr; // no free space, cannot allocate - return error??
+
+    FreeMem* oldNext = curr->next;
+    void* allocatedSegment = curr;
+
+    size_t remainingSize = curr->size - size;
+
+    if (remainingSize > 0) { // if there is at least 1 block of free memory in this chunk left over after allocation
+        curr = (FreeMem*) ((char*)curr + size*MEM_BLOCK_SIZE); // moving the pointer to new start of free memory
+        curr->size = remainingSize;
+        curr->next = oldNext;
+        if(prev) prev->next = curr;
+        else freeMemHead = curr;
+    }
+    else { // allocated the whole segment size
+        if (prev) prev->next = oldNext;
+        else freeMemHead = oldNext;
+    }
+
+    *(size_t*) allocatedSegment = size; // ovo mi je bilo bolno da skontam :(
+
+    return (void*)((char*)allocatedSegment + sizeof(size_t));
+}
+
+int MemoryAllocator::kmem_free(void *ptr) {
+    return -1;
+}
+
+void MemoryAllocator::kinit() {
+    freeMemHead = (FreeMem*) HEAP_START_ADDR;
+    freeMemHead->next = nullptr;
+    freeMemHead->size = ((const char*) HEAP_END_ADDR - (const char*) HEAP_START_ADDR) / MEM_BLOCK_SIZE;
+}
