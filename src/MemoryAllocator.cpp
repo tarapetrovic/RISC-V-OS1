6646574c454f1d4e6ac3e8a571e7987cc8cb765e
Tara <tara.petrovic03@gmail.com>
2026-06-15 19:16:03 -0700

    kmem_free done

--- src/MemoryAllocator.cpp
+++ src/MemoryAllocator.cpp
@@ -36,7 +36,44 @@ void *MemoryAllocator::kmem_alloc(size_t size) {
 }
 
 int MemoryAllocator::kmem_free(void *ptr) {
-    return -1;
+    if (ptr == nullptr) return -1; // error code for nullptr
+    if ((const char*)ptr < (const char*)HEAP_START_ADDR || (const char*)ptr >= (const char*)HEAP_END_ADDR) return -2; // error code for out of bounds
+
+    FreeMem* segToFree = (FreeMem*)((char*)ptr - sizeof(size_t));
+    segToFree->size = *(size_t*)segToFree;
+
+    FreeMem* curr = freeMemHead, *prev = nullptr;
+    for (; curr != nullptr; prev = curr, curr = curr->next){
+        if (curr > segToFree) break;
+    }
+
+    //need to check if we are double freeing - if the segment is already in free memory, eg overlapping with prev or curr memory addresses
+    if (prev != nullptr && (char*)segToFree < (char*)prev + prev->size*MEM_BLOCK_SIZE) return -3;
+    if (curr != nullptr && (char*)segToFree + segToFree->size*MEM_BLOCK_SIZE > (char*)curr) return -3;
+
+    segToFree->next = curr;
+    bool merged = false;
+
+    if (curr && (char*)segToFree + segToFree->size*MEM_BLOCK_SIZE == (char*)curr) { // merge with right neighbor
+        segToFree->size = segToFree->size + curr->size;
+        segToFree->next = curr->next;
+        if (prev) prev->next = segToFree;
+        else freeMemHead = segToFree;
+        merged = true;
+    }
+
+    if (prev && (char*)prev + prev->size*MEM_BLOCK_SIZE == (char*)segToFree) { // merge with left neighbor
+        prev->size = prev->size + segToFree->size;
+        prev->next = segToFree->next;
+        merged = true;
+    }
+
+    if (!merged) { // didnt merge with any neighbours, needs to be inserted into the list
+        if (prev) prev->next = segToFree;
+        else freeMemHead = segToFree;
+    }
+
+    return 0;
 }
 
 void MemoryAllocator::kinit() {
