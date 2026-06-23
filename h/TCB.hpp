7214262f3d6f45634bc30706f89f74cdb22dc7b4
Tara <tara.petrovic03@gmail.com>
2026-06-22 20:02:29 -0700

    changed the code from 7. lesson a bit, trying to adjust it to the project

--- h/TCB.hpp
+++ h/TCB.hpp
@@ -8,12 +8,12 @@
 
 #include "../lib/hw.h"
 // #include "scheduler.hpp"
-/*
+
 // Thread Control Block
 class TCB
 {
 public:
-    ~TCB() { delete[] stack; }
+    ~TCB() {} // destruktor ne treba da dealocira stek vec korisnik, takodje za brisanje niti je syscall thread_exit
 
     bool isFinished() const { return finished; } // za sad nek bude ovako, Jaksa ima enum stanja running finished blocked sleeping, mozda kasnije
 
@@ -22,28 +22,26 @@ public:
     uint64 getTimeSlice() const { return timeSlice; }
 
     using Body = void (*)(void*);
-    using Argument = void*;
+    using Argument = void*; // razmisli zelis li ovo da koristis
 
-    static TCB *createThread(Body body, Argument arg, vpid* stack_space);
+    static TCB *createThread(Body body, Argument arg, void* stack_space);
 
     static void yield();
 
     static TCB *running;
 
-    // do ovde sam stigla sa proverom sta mi sve treba!! - zato je ostatak u komentaru, vraticu se na ovo
-// ----------------------------------------------------------------------
-
 private:
-    TCB(Body body, uint64 timeSlice) :
+    TCB(Body body, uint64 timeSlice, void* arg, void* stack_space) :
             body(body),
-            stack(body != nullptr ? new uint64[STACK_SIZE] : nullptr),
+            stack(body != nullptr ? (char*) stack_space : nullptr), // main function has nullptr for its body, and it doesnt need stack
             context({(uint64) &threadWrapper,
-                     stack != nullptr ? (uint64) &stack[STACK_SIZE] : 0
+                     stack != nullptr ? (uint64)((char*)stack + DEFAULT_STACK_SIZE) : 0 // na adresu stack dodamo default velicinu steka da dobijemo najvisu adresu, od koje zap pocinje stek pa raste na dole
                     }),
             timeSlice(timeSlice),
+            arg(arg),
             finished(false)
     {
-        if (body != nullptr) { Scheduler::put(this); }
+        if (body != nullptr) { Scheduler::put(this); } // check if its not main, then add it to scheduler
     }
 
     struct Context
@@ -53,7 +51,9 @@ private:
     };
 
     Body body;
-    uint64 *stack;
+    void* stack; // da li je void* dobar tip? ili bolje char*?
+    // uint64 *stack;
+    void* arg; // ili Argument arg
     Context context;
     uint64 timeSlice;
     bool finished;
@@ -68,9 +68,8 @@ private:
 
     static uint64 timeSliceCounter;
 
-    static uint64 constexpr STACK_SIZE = 1024;
-    static uint64 constexpr TIME_SLICE = 2;
+    // static uint64 constexpr STACK_SIZE = 1024;
+    // static uint64 constexpr TIME_SLICE = 2;
 };
-*/
 
 #endif //PROJECT_BASE_V1_1_TCB_HPP
