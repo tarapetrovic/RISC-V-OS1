0ca2bad6db3f2c5140b303759c4a20de9786f41f
Tara <tara.petrovic03@gmail.com>
2026-06-22 20:21:24 -0700

    Added only skeleton of functions, will implement later

--- src/TCB.cpp
+++ src/TCB.cpp
@@ -4,3 +4,29 @@
 // Kod preuzet sa 7. vezbi
 
 #include "../h/TCB.hpp"
+
+TCB *TCB::running = nullptr;
+uint64 TCB::timeSliceCounter = 0;
+
+TCB *TCB::createThread(TCB::Body body, TCB::Argument arg, void *stack_space) {
+    return nullptr;
+}
+
+
+void TCB::yield() {
+
+}
+
+void TCB::threadWrapper() {
+
+}
+
+
+void TCB::contextSwitch(TCB::Context *oldContext, TCB::Context *runningContext) {
+
+}
+
+
+void TCB::dispatch() {
+
+}
