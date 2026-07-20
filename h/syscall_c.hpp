ee5a650db63e6d7f9af7cc3e08a9153b69e13ec8
Tara <tara.petrovic03@gmail.com>
2026-07-19 22:57:59 -0700

    Added C API syscalls for Semaphores

--- h/syscall_c.hpp
+++ h/syscall_c.hpp
@@ -19,5 +19,12 @@
     int thread_create(thread_t* handle, void(*start_routine)(void*), void* arg);
     int thread_exit();
 
+    int sem_open(sem_t* handle, unsigned init);
+    int sem_close(sem_t handle);
+    int sem_wait(sem_t id);
+    int sem_signal(sem_t id);
+    int sem_wait_n(sem_t id, unsigned n);
+    int sem_signal_n(sem_t id, unsigned n);
+
 
 #endif //PROJECT_BASE_V1_1_SYSCALL_C_HPP
