04efe8da12c0e0510997040075828971e0403057
Tara <tara.petrovic03@gmail.com>
2026-07-20 21:45:43 -0700

    Implementation of 20 points tier functions, destructor, periodic thread and console are empty, TODO

--- src/syscall_cpp.cpp
+++ src/syscall_cpp.cpp
@@ -3,3 +3,61 @@
 //
 
 #include "../h/syscall_cpp.hpp"
+
+// --- Thread ---
+
+Thread::Thread(void (*body)(void *), void *arg) {
+    myHandle = nullptr;
+    this->body = body;
+    this->arg = arg;
+}
+
+Thread::Thread() {
+    myHandle = nullptr;
+    body = &wrapper;
+    arg = this;
+}
+
+Thread::~Thread() noexcept {
+    // ?????? TODO
+}
+
+int Thread::start() {
+    return thread_create(&myHandle, body, arg);
+}
+
+void Thread::dispatch() {
+    thread_dispatch();
+}
+
+int Thread::sleep(time_t t) {
+    return -1;   // TODO - return time_sleep(t);
+}
+
+void Thread::wrapper(void* thr) {
+    ((Thread*)thr)->run();
+}
+
+// --- Semaphore ---
+
+Semaphore::Semaphore(unsigned init) {
+    sem_open(&myHandle, init);
+}
+
+Semaphore::~Semaphore() {
+    sem_close(myHandle);
+}
+
+int Semaphore::wait() {
+    return sem_wait(myHandle);
+}
+
+int Semaphore::signal() {
+    return sem_signal(myHandle);
+}
+
+// --- Periodic Thread ---
+// TODO asinhrona
+
+// --- Console ---
+// TODO na kraju
