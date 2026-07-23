771fb1e1c64d37c8a6b6152cc9008829a1ccbbab
Tara <tara.petrovic03@gmail.com>
2026-07-22 19:54:57 -0700

    tesing step 9 - timer preemption and time sharing

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,76 @@
 // Kod preuzet sa 7. vezbi
 
 #include "../h/Riscv.hpp"
-// #include "../lib/console.h"
+#include "../lib/console.h"
 #include "../h/MemoryAllocator.hpp"
 #include "../h/syscall_c.hpp"
 #include "../h/TCB.hpp"
 // #include "../h/syscall_cpp.hpp"
 
-
+//// ------------ MAIN FOR TESTING OFFICIAL TESTS (20 POINTS) ------------------
 extern void userMain(); // defined in userMain.cpp
 
-// wrapper so we can launch userMain as a thread body (needs void(*)(void*) shape)
+ // wrapper so we can launch userMain as a thread body (needs void(*)(void*) shape)
 static void userMainWrapper(void*) {
     userMain();
 }
 
-int main() {
+//static void printStr(const char* s) { while (*s) { __putc(*s); s++; } }
+//static void printNum(uint64 n) {
+//    if (n >= 10) printNum(n / 10);
+//    __putc('0' + (n % 10));
+//}
+//
+//static const uint64 WORK = 50000000;
+//
+//static volatile uint64 counterA = 0, counterB = 0;
+//static volatile uint64 bSeenByA = 0;      // B's progress, sampled mid-run by A
+//static volatile bool doneA = false, doneB = false;
+//
+//// NOTE: neither worker ever calls thread_dispatch()
+//static void spinA(void*) {
+//    for (uint64 i = 0; i < WORK; i++) {
+//        counterA++;
+//        if (i == WORK / 2) bSeenByA = counterB;   // snapshot at halfway
+//    }
+//    doneA = true;
+//}
+//
+//static void spinB(void*) {
+//    for (uint64 i = 0; i < WORK; i++) { counterB++; }
+//    doneB = true;
+//}
+//extern volatile uint64 tickCount;
+//extern volatile uint64 preemptCount;
 
+int main() {
+//    MemoryAllocator::kinit();
+//    Riscv::w_stvec((uint64)&Riscv::supervisorTrap);
+//    TCB::running = TCB::createThread(nullptr, nullptr, nullptr);
+//
+//    thread_t a, b;
+//    thread_create(&a, spinA, nullptr);
+//    thread_create(&b, spinB, nullptr);
+//
+//    // main is supervisor mode => never preempted => must yield explicitly
+//    while (!doneA || !doneB) { thread_dispatch(); }
+//
+//    printStr("\ncounterA = "); printNum(counterA);
+//    printStr("\ncounterB = "); printNum(counterB);
+//    printStr("\nB progress seen at A's midpoint = "); printNum(bSeenByA);
+//    printStr(bSeenByA > 0 ? "  [PREEMPTION WORKS]\n" : "  [NO PREEMPTION]\n");
+//
+//    printStr("\nticks = "); printNum(tickCount);
+//    printStr("\npreemptions = "); printNum(preemptCount);
+//    return 0;
+
+
+
+
+
+//// ------------ MAIN FOR TESTING OFFICIAL TESTS (20 POINTS) ------------------
     MemoryAllocator::kinit();
     Riscv::w_stvec((uint64)&Riscv::supervisorTrap);
-
     TCB::running = TCB::createThread(nullptr, nullptr, nullptr); // main thread
 
     // create a thread over userMain
@@ -36,44 +87,5 @@ int main() {
 
     return 0;
 
-
-    /* Riscv::w_stvec((uint64)&Riscv::supervisorTrap);
-    //Riscv::ms_sstatus(Riscv::SSTATUS_SIE);
-    __asm__ volatile("ecall");
-    __putc('O'); __putc('K');
-*/
-    /*
-    TCB *threads[5];
-
-    threads[0] = TCB::createThread(nullptr);
-    TCB::running = threads[0];
-
-    threads[1] = TCB::createThread(workerBodyA);
-    printString("ThreadA created\n");
-    threads[2] = TCB::createThread(workerBodyB);
-    printString("ThreadB created\n");
-    threads[3] = TCB::createThread(workerBodyC);
-    printString("ThreadC created\n");
-    threads[4] = TCB::createThread(workerBodyD);
-    printString("ThreadD created\n");
-
-    Riscv::w_stvec((uint64) &Riscv::supervisorTrap);
-    Riscv::ms_sstatus(Riscv::SSTATUS_SIE);
-
-    while (!(threads[1]->isFinished() &&
-             threads[2]->isFinished() &&
-             threads[3]->isFinished() &&
-             threads[4]->isFinished()))
-    {
-        TCB::yield();
-    }
-
-    for (auto &thread: threads)
-    {
-        delete thread;
-    }
-    printString("Finished\n");
-*/
-    return 0;
 }
 
