3d02802c81acfa16a4dcaec53e5eb7c5a7062290
Tara <tara.petrovic03@gmail.com>
2026-07-22 22:31:52 -0700

    added sleeping mechanisms

--- h/Scheduler.hpp
+++ h/Scheduler.hpp
@@ -6,7 +6,9 @@
 #ifndef PROJECT_BASE_V1_1_SCHEDULER_HPP
 #define PROJECT_BASE_V1_1_SCHEDULER_HPP
 
+#include "../lib/hw.h"
 #include "../h/List.hpp"
+#include "../h/SleepingList.hpp"
 
 class TCB;
 
@@ -14,11 +16,14 @@ class Scheduler
 {
 private:
     static List<TCB> readyThreadQueue;
+    static SleepingList sleepingThreadQueue;
 
 public:
     static TCB *get();
-
     static void put(TCB *ccb);
+
+    static int putToSleep(time_t time);
+    static void updateSleeping();
 
 };
 
--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -3,8 +3,10 @@
 //
 
 #include "../h/Scheduler.hpp"
+#include "../h/TCB.hpp"
 
 List<TCB> Scheduler::readyThreadQueue;
+SleepingList Scheduler::sleepingThreadQueue;
 
 TCB *Scheduler::get()
 {
@@ -14,4 +16,24 @@ TCB *Scheduler::get()
 void Scheduler::put(TCB *ccb)
 {
     readyThreadQueue.addLast(ccb);
+}
+
+int Scheduler::putToSleep(time_t time) {
+    if (time == 0) { return 0; } // dont do anything, not really sleeping
+    TCB* running = TCB::running;
+    if (running == nullptr || running->isFinished()) { return -1; }
+
+    running->setBlocked(true); // sleeping and blocking are sharing a variable, think about dividing them to blocked and sleeping, or if this is ok
+    sleepingThreadQueue.insert(running, time);
+    TCB::dispatch(); // kernel-internal dispatch, thread_dispatch would be a syscall from kernel, unnecessary
+    return 0;
+}
+
+void Scheduler::updateSleeping() {
+    sleepingThreadQueue.tick();
+    TCB* woken;
+    while ((woken = sleepingThreadQueue.removeExpired()) != nullptr) {
+        woken->setBlocked(false);
+        put(woken); // wake up and put in scheduler
+    }
 }
